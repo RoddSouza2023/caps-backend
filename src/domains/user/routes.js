5c218e85ac91741799a1a7034d97aeffcc3bb78b
Rodrigo Souza <roddsouza@hotmail.com>
2023-09-16 17:41:06 -0400

    set up authentication verification with middleware

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { createNewUser, authenticateUser } = require("./controller");
+const auth = require("./../../middleware/auth");
+
+//protected route
+router.get("/private_data", auth, (req,res) => {
+  res
+    .status(200)
+    .send(`You're logged in as ${req.currentUser.email}`);
+});
 
 //SignIn
 router.post("/", async (req, res) => {
