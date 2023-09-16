e076019f6648c865b3b165d44be31bdf34995715
Rodrigo Souza <roddsouza@hotmail.com>
2023-09-16 15:33:44 -0400

    initial commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# Fullstack ACC Capstone Boilerplate
-
-2302 Backend Boilerplate starter
-
-for Front ent use 2302-FE branch
---
+++ src/app.js
@@ -0,0 +1,13 @@
+require("./config/db");
+
+const express = require("express");
+const bodyParser = express.json;
+const cors = require("cors")
+
+//create server app
+const app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
+module.exports = app;
